099428028ca4d2d565742ff0f6c0d3b718ea77c4
Erhan Toktamış <erhantoktamis@icloud.com>
2024-02-20 18:53:56 +0300

    gereksiz değişken kullanımı giderildi.

--- Week 1 /DikUcgenHipotenusBulanProgram/src/Main.java
+++ Week 1 /DikUcgenHipotenusBulanProgram/src/Main.java
@@ -4,16 +4,14 @@ public class Main {
 
         double a, b, c, cevre, alan;
 
-        Scanner kisaKenar = new Scanner(System.in);
-        Scanner uzunKenar = new Scanner(System.in);
-        Scanner hipotenus = new Scanner(System.in);
+        Scanner kenarInput = new Scanner(System.in);
 
         System.out.print("Lütfen a değerini giriniz : " );
-        a = kisaKenar.nextDouble();
+        a = kenarInput.nextDouble();
         System.out.print("Lütfen b değer giriniz : " );
-        b = uzunKenar.nextDouble();
+        b = kenarInput.nextDouble();
         System.out.print("Lütfen c değer giriniz : " );
-        c = hipotenus.nextDouble();
+        c = kenarInput.nextDouble();
 
         cevre = (a + b + c) / 2;
         alan  =  Math.sqrt(cevre * (cevre - a)* (cevre - b) * (cevre - c));
