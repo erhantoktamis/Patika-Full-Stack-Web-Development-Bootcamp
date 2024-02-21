48e8db665c4e78313f2655865fbd79d89bf6435d
Erhan Toktamış <erhantoktamis@icloud.com>
2024-02-21 16:10:33 +0300

    Yorum satırları eklendi.

--- Week 1 /VKI/src/Main.java
+++ Week 1 /VKI/src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
     public class Main {
     public static void main(String[] args) {
 
-        double boy, kilo, vki;
-        Scanner input = new Scanner(System.in);
+        double boy, kilo, vki; // Değişkenler tanımlandı.
+        Scanner input = new Scanner(System.in); // Kullanıcıdan veri almak için Scanner sınıfı tanımlandı.
         System.out.print("Lütfen boyunuzu giriniz (m) : ");
         boy = input.nextDouble();
         System.out.print("Lütfen kilonuzu giriniz (kg) : ");
         kilo = input.nextDouble();
 
-        vki = kilo / (boy * boy);
+        vki = kilo / (boy * boy); // Matematiksel işlem ile vücut kitle indeksi hesaplandı.
         System.out.print("Vücut Kitle İndeksiniz : " + vki);
 
     }
